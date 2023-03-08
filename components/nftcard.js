bf9e58d843d150367d5b21aff356f8ff256f4fb2
robel.michael <robel.michael@onelab.se>
2023-03-08 22:35:10 +0100

    tweak

--- components/nftcard.js
+++ components/nftcard.js
@@ -12,7 +12,7 @@ const Nftcard = ({ ...props }) => {
       }}
       className="animate-animate w-full md:w-5/12 cursor-pointer shadow-gl shadow-[#185ee041] h-auto bg-gradient-to-r from-blue-400 via-blue-300 to-pink-300 bg-opacity-10 dark:bg-gradient-to-r dark:from-blue-800 dark:via-blue-700 dark:to-pink-500  p-10 rounded-xl"
     >
-      <div className="   flex flex-col gap-4">
+      <div className="flex flex-col gap-4">
         <img className="h-62 rounded-md" src={image_preview_url} />
         <span className=" text-3xl font-bold  ">{name} </span>
 
